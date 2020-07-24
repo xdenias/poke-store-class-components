64648e6551eec97788a1fe180030eeaf9bb38628
Denilson Santos <denilson.avsantos@hotmail.com>
2020-07-24 14:49:23 -0300

    feat(structure):consuming api and iterate data on screen

--- src/App.js
+++ src/App.js
@@ -6,79 +6,59 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokemons: {},
-      nome: "Squirtle",
-      preco: "R$180,00",
+      pokemons: [],
+      preco: [],
     };
-    // this.listagemHome = this.listagemHome.bind(this);
-    // this.produtoRedirecionamento = this.produtoRedirecionamento.bind(this);
   }
 
-  async requestAPI() {
-    const response = JSON.stringify(await api.get(`/plant`));
-    // console.log(response);
-    this.setState({ pokemons: [...response.data] });
-    console.log(this.state.pokemons);
+  async requestAPI(index) {
+    const response = await (await api.get(`/plant`)).data.pokemon_species;
+    this.setState({ pokemons: response });
+    for (var i = 0; i < this.state.pokemons.length; i++) {
+      this.state.preco[i] = Math.floor(
+        Math.random(this.state.pokemons[i].valueOf) * 100
+      );
+    }
+    return this.state.preco[index];
   }
+
   componentDidMount() {
     this.requestAPI();
   }
+
   render() {
     return (
       <React.Fragment>
-        <body>
-          <header id="box_input">
-            <nav className="input_name">
-              <input></input>
-            </nav>
-          </header>
-          <main id="container">
-            <section className="wrapc">
-              <div className="wrapc_box">
-                <img src="" alt="" />
-                <button className="wrapc_button"></button>
-              </div>
-              <div className="wrapc_box">
-                <img src="" alt="" />
-                <h3>{this.state.nome}</h3>
-                <h3>{this.state.preco}</h3>
-                <button className="wrapc_button"></button>
-              </div>
-              <div className="wrapc_box">
-                <img src="" alt="" />
-                <h3>{this.state.nome}</h3>
-                <h3>{this.state.preco}</h3>
-                <button className="wrapc_button"></button>
-              </div>
-              <div className="wrapc_box">
-                <img src="" alt="" />
-                <h3>{this.statenome}</h3>
-                <h3>{this.statepreco}</h3>
-                <button className="wrapc_button"></button>
-              </div>
-              <div className="wrapc_box">
-                <img src="" alt="" />
-                <h3>{this.state.nome}</h3>
-                <h3>{this.state.preco}</h3>
-                <button className="wrapc_button"></button>
-              </div>
-              <div className="wrapc_box">
-                <img src="" alt="" />
-                <h3>{this.state.nome}</h3>
-                <h3>{this.state.preco}</h3>
-                <button className="wrapc_button"></button>
-              </div>
-            </section>
-            <section className="wrapd">
-              <h1>Carrinho</h1>
-              <aside className="Itens">
-                <h4>{this.state.nome}</h4>
-              </aside>
-              <h3>Total</h3>
-              <button>Finalizar</button>
-            </section>
-          </main>
-        </body>
+        <header id="box_input">
+          <nav className="input_name">
+            <input></input>
+          </nav>
+        </header>
+        <main id="container">
+          <section className="wrapc">
+            <div className="wrapc_box">
+              <img src="" alt="" />
+              <button className="wrapc_button">{this.state.preco}</button>
+            </div>
+
+            {this.state.pokemons.map((valor, index, x) => {
+              return (
+                <div className="wrapc_box" key={valor.name}>
+                  <img src="" alt="" />
+                  <h1>{valor.name}</h1>
+                  <h4>precos</h4>
+                  <button className="wrapc_button"></button>
+                </div>
+              );
+            })}
+          </section>
+          <section className="wrapd">
+            <h1>Carrinho</h1>
+            <aside className="Itens">{/* <h4>{this.pokemons[0]}</h4> */}</aside>
+            <h3>Total</h3>
+            <button>Finalizar</button>
+          </section>
+        </main>
       </React.Fragment>
     );
   }
