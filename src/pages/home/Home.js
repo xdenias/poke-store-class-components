2d330655503508269244f5a7ca638c315cf50021
Denilson Santos <denilson.avsantos@hotmail.com>
2020-07-26 19:40:57 -0300

    feat(structure):structuring and styling styling home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -4,14 +4,22 @@ import { Link } from "react-router-dom";
 const Home = () => {
   return (
     <React.Fragment>
-      <ul>
+      <main id="content">
+        <section className="content_plant">
+          <h1>Pokemon tipo planta</h1>
+        </section>
+        <section className="content_water">
+          <h1>Pokemon tipo água</h1>
+        </section>
+      </main>
+      {/* <ul>
         <li>
           <Link to="/loja1">Loja 1</Link>
         </li>
         <li>
           <Link to="/loja2">Loja 2</Link>
         </li>
-      </ul>
+      </ul> */}
     </React.Fragment>
   );
 };
